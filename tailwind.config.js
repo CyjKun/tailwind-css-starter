2979470218be1cce01678e330a511a8db2bd565c
Christian Johnathan Baquiran <lightningpatapon@gmail.com>
2021-05-02 03:53:17 +0800

    Add design customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
+        },
+      },
+      fontFamily: {
+        headline: "Poppins, sans-serif",
+      },
+    },
   },
   variants: {
     extend: {
-      backgroundColor:["active"],
+      backgroundColor: ["active"],
     },
   },
   plugins: [],
-}
+};
