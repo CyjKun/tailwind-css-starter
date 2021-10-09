6783eac973626df3cf60356af3289bab560d6367
cjbaquiran <cjbbaquiran@unionbankph.com>
2021-10-09 19:36:04 +0800

    Move img folder to src folder and update img urls

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ export default function App() {
       <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5">
         <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
           <div className="xl:max-w-xl">
-            <img className="h-10" src="/img/logo-brand.svg" alt="Workcation" />
+            <img className="h-10" src="/src/img/logo-brand.svg" alt="Workcation" />
             <img
               className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
-              src="./img/beach-work.jpg"
+              src="./src/img/beach-work.jpg"
               alt="Woman workcationing on the beach"
             />
             <h1 className="mt-6 text-2xl font-headline tracking-tight font-semibold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
@@ -36,7 +36,7 @@ export default function App() {
         <div className="hidden relative lg:block 2xl:col-span-3">
           <img
             className="absolute inset-0 w-full h-full object-cover object-center"
-            src="./img/beach-work.jpg"
+            src="/src/img/beach-work.jpg"
             alt="Woman workcationing on the beach"
           />
         </div>
